1f8029093f7a6b99d3385b0827a0fddf0cbee731
Bert Hoogsteyns <bhoogsteyns@fastmail.fm>
2023-12-11 11:33:42 +0100

    started migrating from astrovim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- local Util = require("lazyvim.util")
+
+map("n", "<leader>o", function()
+  if vim.bo.filetype == "neo-tree" then
+    vim.cmd.wincmd("p")
+  else
+    vim.cmd.Neotree("focus")
+  end
+end, { desc = "Toggle neotree focus" })
